b4bcdd40e260ffe1c06108bbbfd982d01afc457d
Vaibhav <vaibhav1180@gmail.com>
2021-06-13 20:46:45 +0530

    step-3 code in progress

--- src/Cursor.js
+++ src/Cursor.js
@@ -1,12 +1,105 @@
 import Link from 'Link';
-import { useRef } from 'react';
+import { useEffect, useRef } from 'react';
 import getImage from 'util/getImage';
 import './App.css';
 
 const Cursor = () => {
+  const delay = 18;
   const dot = useRef(null);
   const dotOutline = useRef(null);
   const list = useRef(null);
+
+  const cursorVisible = useRef(true);
+  const cursorEnlarged = useRef(false);
+
+  const endX = useRef(window.innerWidth / 2);
+  const endY = useRef(window.innerHeight / 2);
+  const _x = useRef(0);
+  const _y = useRef(0);
+
+  const requestRef = useRef(null);
+
+  useEffect(() => {
+    document.addEventListener('mousedown', mouseOverEvent);
+    document.addEventListener('mouseup', mouseOutEvent);
+    document.addEventListener('mousemove', mouseMoveEvent);
+    document.addEventListener('mouseenter', mouseEnterEvent);
+    document.addEventListener('mouseleave', mouseLeaveEvent);
+
+    animateDotOutline();
+
+    return () => {
+      document.removeEventListener('mousedown', mouseOverEvent);
+      document.removeEventListener('mouseup', mouseOutEvent);
+      document.removeEventListener('mousemove', mouseMoveEvent);
+      document.removeEventListener('mouseenter', mouseEnterEvent);
+      document.removeEventListener('mouseleave', mouseLeaveEvent);
+
+      cancelAnimationFrame(requestRef.current);
+    };
+  }, []);
+
+  const toggleCursorVisibility = () => {
+    if (cursorVisible.current) {
+      dot.current.style.opacity = 1;
+      dotOutline.current.style.opacity = 1;
+    } else {
+      dot.current.style.opacity = 0;
+      dotOutline.current.style.opacity = 0;
+    }
+  };
+
+  const toggleCursorSize = () => {
+    if (cursorEnlarged.current) {
+      dot.current.style.transform = 'translate(-50%, -50%) scale(0.75)';
+      dotOutline.current.style.transform = 'translate(-50%, -50%) scale(1.5)';
+    } else {
+      dot.current.style.transform = 'translate(-50%, -50%) scale(1)';
+      dotOutline.current.style.transform = 'translate(-50%, -50%) scale(1)';
+    }
+  };
+
+  const mouseOverEvent = () => {
+    console.log('mouseover');
+    cursorEnlarged.current = true;
+    toggleCursorSize();
+  };
+
+  const mouseOutEvent = () => {
+    cursorEnlarged.current = false;
+    toggleCursorSize();
+  };
+
+  const mouseEnterEvent = () => {
+    cursorVisible.current = true;
+    toggleCursorVisibility();
+  };
+
+  const mouseLeaveEvent = () => {
+    cursorVisible.current = false;
+    toggleCursorVisibility();
+  };
+
+  const mouseMoveEvent = e => {
+    cursorVisible.current = true;
+    toggleCursorVisibility();
+
+    endX.current = e.pageX;
+    endY.current = e.pageY;
+
+    dot.current.style.top = endY.current + 'px';
+    dot.current.style.left = endX.current + 'px';
+  };
+
+  const animateDotOutline = () => {
+    _x.current += (endX.current - _x.current) / delay;
+    _y.current += (endY.current - _y.current) / delay;
+
+    dotOutline.current.style.top = _y.current + 'px';
+    dotOutline.current.style.left = _x.current + 'px';
+
+    requestRef.current = requestAnimationFrame(animateDotOutline);
+  };
 
   return (
     <>
@@ -14,9 +107,15 @@ const Cursor = () => {
       <div ref={dot} className="cursor-dot"></div>
 
       <div ref={list} className="links-container">
-        {[...Array(4).keys()].map(i => (
-          <Link key={i} src={getImage(i + 1)} />
-        ))}
+        {/* {[...Array(4).keys()].map(i => (
+          <Link
+            key={i}
+            src={getImage(i + 1)}
+            onMouseOver={mouseOverEvent}
+            onMouseOut={mouseOutEvent}
+          />
+        ))} */}
+        <Link onMouseOver={mouseOverEvent} onMouseOut={mouseOutEvent} />
       </div>
     </>
   );
--- src/Link.js
+++ src/Link.js
@@ -1,5 +1,28 @@
-const Link = ({ src }) => {
-  return <img src={src} />;
+import getImage from 'util/getImage';
+
+const Link = ({ mouseOverEvent, mouseOutEvent, src }) => {
+  return (
+    <>
+      {[...Array(4).keys()].map(i => (
+        <a
+          key={i}
+          href="#!"
+          onMouseOver={mouseOverEvent}
+          onMouseOut={mouseOutEvent}
+        >
+          Count {i}
+        </a>
+      ))}
+    </>
+  );
 };
 
 export default Link;
+
+// <img
+//   alt="images"
+//   key={i}
+//   onMouseOver={mouseOverEvent}
+//   onMouseOut={mouseOutEvent}
+//   src={getImage(i + 1)}
+// />
